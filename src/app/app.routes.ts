ad1b73186946094fffdabea2f12c1726628ee35d
VictoriaReyes <reyedvictoria1803@gmail.com>
2024-09-12 14:47:59 -0600

    primer commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { KraussMaffeiMC6Component } from './krauss-maffei-mc6/krauss-maffei-mc6.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: HomeComponent
+    },
+    {
+        path: 'KraussMaffeiMC6',
+        component: KraussMaffeiMC6Component
+    }
+
+];
---
+++ src/app/krauss-maffei-mc6/krauss-maffei-mc6.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-krauss-maffei-mc6',
+  standalone: true,
+  imports: [],
+  templateUrl: './krauss-maffei-mc6.component.html',
+  styleUrl: './krauss-maffei-mc6.component.css'
+})
+export class KraussMaffeiMC6Component {
+
+}
