d05fb66c0575d8f4e168d6d14f24b9b66bbe97e6
VictoriaReyes <reyedvictoria1803@gmail.com>
2024-09-19 13:52:14 -0600

    avances

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { KraussMaffeiMC6Component } from './Forms/krauss-maffei-mc6/krauss-maffei-mc6.component';
 import { LoginComponent } from './login/login.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
 export const routes: Routes = [
     {
         path: '',
@@ -15,5 +16,7 @@ export const routes: Routes = [
     {
         path:'Login',
         component: LoginComponent
+    },
+    { path: 'Sidebar', component: SidebarComponent
     }
 ];
--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -1,30 +1,22 @@
-import { Component, ViewChild } from '@angular/core';
+import { Component } from '@angular/core';
 import { SidebarModule } from 'primeng/sidebar';
 import { ButtonModule } from 'primeng/button';
-import { RippleModule } from 'primeng/ripple';
 import { AvatarModule } from 'primeng/avatar';
-import { StyleClassModule } from 'primeng/styleclass';
-import { Sidebar } from 'primeng/sidebar';
-import { CommonModule } from '@angular/common';
-
-
 @Component({
   selector: 'app-sidebar',
   standalone: true,
   imports: [
-    SidebarModule, ButtonModule, RippleModule,
-    CommonModule,
-    AvatarModule, StyleClassModule
+    SidebarModule,
+    ButtonModule,
+    AvatarModule
   ],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.css'
 })
 export class SidebarComponent {
-  @ViewChild('sidebarRef') sidebarRef!: Sidebar;
 
-  closeCallback(e: Event): void {
-      this.sidebarRef.close(e);
-  }
+  sidebarVisible: boolean = true;
 
-  sidebarVisible: boolean = false;
+
+
 }
