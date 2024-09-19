bbd1fa1f32430264f3c2e113fe621a03074d860d
ReneArteaga22 <rene_arteaga22@outlook.com>
2024-09-19 09:19:24 -0600

    login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { KraussMaffeiMC6Component } from './krauss-maffei-mc6/krauss-maffei-mc6.component';
 import { FormDinamicComponent } from './form-dinamic/form-dinamic.component';
+import { LoginComponent } from './login/login.component';
 export const routes: Routes = [
     {
         path: '',
@@ -14,6 +15,10 @@ export const routes: Routes = [
     {
         path: 'FormDinamic',
         component: FormDinamicComponent
+    },
+    {
+        path:'Login',
+        component: LoginComponent
     }
 
 ];
