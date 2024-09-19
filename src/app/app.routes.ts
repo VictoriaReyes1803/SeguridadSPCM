f4dbb4e66c21a4432bf39a9110703a9b46bc71e1
VictoriaReyes <reyedvictoria1803@gmail.com>
2024-09-19 12:12:45 -0600

    avances

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,7 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-<<<<<<< HEAD
-import { KraussMaffeiMC6Component } from './krauss-maffei-mc6/krauss-maffei-mc6.component';
-import { FormDinamicComponent } from './form-dinamic/form-dinamic.component';
-import { LoginComponent } from './login/login.component';
-=======
 import { KraussMaffeiMC6Component } from './Forms/krauss-maffei-mc6/krauss-maffei-mc6.component';
-
->>>>>>> 89e1f6e777046b489f37ad8ce4cfb9022a278248
+import { LoginComponent } from './login/login.component';
 export const routes: Routes = [
     {
         path: '',
@@ -16,16 +10,10 @@ export const routes: Routes = [
     {
         path: 'KraussMaffeiMC6',
         component: KraussMaffeiMC6Component
-<<<<<<< HEAD
     },
-    {
-        path: 'FormDinamic',
-        component: FormDinamicComponent
-    },
+  
     {
         path:'Login',
         component: LoginComponent
-=======
->>>>>>> 89e1f6e777046b489f37ad8ce4cfb9022a278248
     }
 ];
