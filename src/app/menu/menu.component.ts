1990a38832368ccbd4f3eee5e59a434f033c7610
ReneArteaga22 <rene_arteaga22@outlook.com>
2024-10-23 17:56:52 -0600

    prueba

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
-
+import { CardModule } from 'primeng/card';
 import { SidebarComponent } from '../sidebar/sidebar.component';
 import { ButtonModule } from 'primeng/button';
+import { DropdownModule } from 'primeng/dropdown';
+import { ToolbarComponent } from '../toolbar/toolbar.component';
+
 
 @Component({
   selector: 'app-menu',
   standalone: true,
-  imports: [
-  
+  imports: [ 
+    ToolbarComponent,
+    DropdownModule,
+    CardModule,
     SidebarComponent,
     ButtonModule
   ],
@@ -15,5 +20,7 @@ import { ButtonModule } from 'primeng/button';
   styleUrl: './menu.component.css'
 })
 export class MenuComponent {
+
+
 
 }
