68a3d329be792af653aac3e9baf14b984288b998
VictoriaReyes <reyedvictoria1803@gmail.com>
2024-11-13 13:47:36 -0600

    avances

--- src/app/Models/Formatos.ts/FP1217.ts
+++ src/app/Models/Formatos.ts/FP1217.ts
@@ -8,7 +8,35 @@ export interface FP1217 {
     Num_Cav_real: number;
     porc_pigmento: number;
     editado_por: string;
-
-
+    Velocidad_avance: number;
+    Velocidad_colocacion: number;
+    Fuerza_Apoyo: number;
+    posicion: number;
+    velocidad: number;
+    inyeccion1s: number;
+    inyeccion2s: number;
+    inyeccion1v: number;
+    inyeccion2v: number;
+    
+    inyeccion3s: number;
+    inyeccion4s: number;
+    inyeccion3v: number;
+    inyeccion4v: number;
+    inyeccion5v: number;
+    inyeccion5s: number;
+    s1: number;
+    t1: number;
+    p1: number;
+    s2: number;
+    t2: number;
+    p2: number;
+    s3: number;
+    t3: number;
+    p3: number;
+    s4: number;
+    t4: number;
+    p4: number;
+    v_p_switchover_release_pos: number;
+    retrocesomm1: number;
     
 }
